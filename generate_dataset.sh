1d51b2b75a3b8479aeb45257e7d28f038451b396
LoaneShark <loane2@illinois.edu>
2025-06-11 16:48:48 -0500

    Generalize generate_dataset.sh for convenience during future NSBH and generic CBC waveform network training datasets.

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -3,29 +3,53 @@ nsample=${2:-100000} # reproduces the paper
 # nsample=1000 # for test runs
 seed=${3:-1234}
 
-# TODO: Priority 1
-# rescale frequency (f here is actually ~ fM, so NS ranges will be smaller --> find minimum based on LIGO sensitivity?)
-# -----> try training VAE now and see what it looks like
 
-# TODO: Priority 2
-# find new way to rescale frequency bins (currently N=640, logarithmic spacing) ? --> training data dependent
-# How to best include massive scalar theories (follow up with Nico on specifics)
+# Black Hole parameters
+M_MIN_BH=5.0
+M_MAX_BH=30.0
+CHI_MIN_BH=-0.99
+CHI_MAX_BH=0.99
+
+# Neutron Star parameters
+M_MIN_NS=0.6
+M_MAX_NS=3.0
+CHI_MIN_NS=-0.1
+CHI_MAX_NS=0.1
+
+# Dimensionless tidal parameters
+L_MAX=5000
+L_MAX=0
+CQ_MIN=0
+CQ_MAX=0
+
+# Compactness and radius (unused)
+#C_MIN_NS=0.1
+#C_MAX_NS=0.4
+#R_MIN_NS=9
+#R_MAX_NS=12
+
 
 if [[ "$systype" == "BH" ]]
 then
   # Point Particle BH parameters
-  M_MIN=5.0
-  M_MAX=30.0
-  CHI_MIN=-0.99
-  CHI_MAX=0.99
+  M_MIN_1=$M_MIN_BH
+  M_MIN_2=$M_MIN_BH
+  M_MAX_1=$M_MAX_BH
+  M_MAX_2=$M_MAX_BH
+  CHI_MIN_1=$CHI_MIN_BH
+  CHI_MIN_2=$CHI_MIN_BH
+  CHI_MAX_1=$CHI_MAX_BH
+  CHI_MAX_2=$CHI_MAX_BH
 
-  # Dimensionless BH tidal parameters
-  L_MIN=0
-  L_MAX=0
-  CQ_MIN=0
-  CQ_MAX=0
-  #CQ_MIN=1
-  #CQ_MAX=1
+  # Dimensionless BH tidal parameters (set to zero)
+  L_MIN_1=0
+  L_MIN_2=0
+  L_MAX_1=0
+  L_MAX_2=0
+  CQ_MIN_1=0
+  CQ_MIN_2=0
+  CQ_MAX_1=0
+  CQ_MAX_2=0
 
   # Relevant frequency range
   F_MIN=0.0004    # 10 Hz (detector limit)
@@ -34,32 +58,118 @@ then
 elif [[ "$systype" == "NS" ]]
 then
   # Point Particle NS parameters
-  M_MIN=0.6
-  M_MAX=3.0
-  CHI_MIN=-0.1
-  CHI_MAX=0.1
+  M_MIN_1=$M_MIN_NS
+  M_MIN_2=$M_MIN_NS
+  M_MAX_1=$M_MAX_NS
+  M_MAX_2=$M_MAX_NS
+  CHI_MIN_1=$CHI_MIN_NS
+  CHI_MIN_2=$CHI_MIN_NS
+  CHI_MAX_1=$CHI_MAX_NS
+  CHI_MAX_2=$CHI_MAX_NS
 
   # Dimensionless NS tidal parameters
-  L_MIN=0
-  #L_MAX=5000
-  L_MAX=0
-  CQ_MIN=0
-  CQ_MAX=0
-  #CQ_MIN=1
-  #CQ_MAX=10
+  L_MIN_1=$L_MIN
+  L_MIN_2=$L_MIN
+  L_MAX_1=$L_MAX
+  L_MAX_2=$L_MAX
+  CQ_MIN_1=$CQ_MIN
+  CQ_MIN_2=$CQ_MIN
+  CQ_MAX_1=$CQ_MAX
+  CQ_MAX_2=$CQ_MAX
 
   # Compactness and radius (unused)
-  #C_MIN=0.1
-  #C_MAX=0.4
-  #R_MIN=9
-  #R_MAX=12
+  #C_MIN_1=$C_MIN_NS
+  #C_MIN_2=$C_MIN_NS
+  #C_MAX_1=$C_MAX_NS
+  #C_MAX_2=$C_MAX_NS
+  #R_MIN_1=$R_MIN_NS
+  #R_MIN_2=$R_MIN_NS
+  #R_MAX_1=$R_MAX_NS
+  #R_MAX_2=$R_MAX_NS
 
   # Relevant frequency range
   # TODO: Double check f_min and f_max for IMRPhenomPv2_NRTidalv2 case
   F_MIN=0.00004   # 10 Hz (detector limit)
   F_MAX=0.018     # IMRPhenomD inspiral cutoff
+elif [[ "$systype" == "NSBH" ]]
+then
+  # Point Particle NS parameters
+  # TODO: Should we limit the BH mass range for NSBH?
+  # For now, we use the same BH parameters as in the BH case.
+  M_MIN_1=$M_MIN_BH
+  M_MIN_2=$M_MIN_NS
+  M_MAX_1=$M_MAX_BH
+  M_MAX_2=$M_MAX_NS
+  CHI_MIN_1=$CHI_MIN_BH
+  CHI_MIN_2=$CHI_MIN_NS
+  CHI_MAX_1=$CHI_MAX_BH
+  CHI_MAX_2=$CHI_MAX_NS
+
+  # Dimensionless NS tidal parameters
+  L_MIN_1=0
+  L_MIN_2=$L_MIN
+  L_MAX_1=0
+  L_MAX_2=$L_MAX
+  CQ_MIN_1=0
+  CQ_MIN_2=$CQ_MIN
+  CQ_MAX_1=0
+  CQ_MAX_2=$CQ_MAX
+
+  # Compactness and radius (unused)
+  #C_MIN_1=1
+  #C_MIN_2=$C_MIN_NS
+  #C_MAX_1=1
+  #C_MAX_2=$C_MAX_NS
+  #R_MIN_1=0
+  #R_MIN_2=$R_MIN_NS
+  #R_MAX_1=0
+  #R_MAX_2=$R_MAX_NS
+
+  # Relevant frequency range
+  # TODO: Determine what the f_min and f_max should be for NSBH
+  F_MIN=0.00004   # 10 Hz (detector limit)
+  F_MAX=0.018     # IMRPhenomD inspiral cutoff
+elif [[ "$systype" == "CBC" ]]
+then
+  # Point Particle NS parameters
+  # TODO: Should we enforce any sort of similarity in the two object masses?
+  # For now, allow mass range to be uniformly distributed across the entire NS and BH mass range.
+  M_MIN_1=$M_MIN_NS
+  M_MIN_2=$M_MIN_NS
+  M_MAX_1=$M_MAX_BH
+  M_MAX_2=$M_MAX_BH
+  # Use BH spin limits for both objects
+  CHI_MIN_1=$CHI_MIN_BH
+  CHI_MIN_2=$CHI_MIN_BH
+  CHI_MAX_1=$CHI_MAX_BH
+  CHI_MAX_2=$CHI_MAX_BH
+
+  # Use NS tidal limits for both objects
+  L_MIN_1=$L_MIN
+  L_MIN_2=$L_MIN
+  L_MAX_1=$L_MAX
+  L_MAX_2=$L_MAX
+  CQ_MIN_1=$CQ_MIN
+  CQ_MIN_2=$CQ_MIN
+  CQ_MAX_1=$CQ_MAX
+  CQ_MAX_2=$CQ_MAX
+
+  # Compactness and radius (unused)
+  #C_MIN_1=$C_MIN_NS
+  #C_MIN_2=$C_MIN_NS
+  #C_MAX_1=$C_MAX_NS
+  #C_MAX_2=$C_MAX_NS
+  #R_MIN_1=$R_MIN_NS
+  #R_MIN_2=$R_MIN_NS
+  #R_MAX_1=$R_MAX_NS
+  #R_MAX_2=$R_MAX_NS
+
+  # Relevant frequency range
+  # TODO: Determine what the f_min and f_max should be for this arbitrary CBC case
+  F_MIN=0.00004   # 10 Hz (detector limit)
+  F_MAX=0.018     # IMRPhenomD inspiral cutoff
 else
-  echo "Invalid systype: please select one of (BH, NS)"
+  echo "Invalid systype: please select one of (BH, NS, NSBH, CBC)"
   exit 1
 fi
 
@@ -69,14 +179,14 @@ python npe/generate_dataset.py \
   --b-ppe -1 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -91,14 +201,14 @@ python npe/generate_dataset.py \
   --b-ppe -2 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -113,14 +223,14 @@ python npe/generate_dataset.py \
   --b-ppe -3 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -135,14 +245,14 @@ python npe/generate_dataset.py \
   --b-ppe -4 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -157,14 +267,14 @@ python npe/generate_dataset.py \
   --b-ppe -5 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -180,14 +290,14 @@ python npe/generate_dataset.py \
   --b-ppe -6 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -202,14 +312,14 @@ python npe/generate_dataset.py \
   --b-ppe -7 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -224,14 +334,14 @@ python npe/generate_dataset.py \
   --b-ppe -8 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -246,14 +356,14 @@ python npe/generate_dataset.py \
   --b-ppe -9 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -268,14 +378,14 @@ python npe/generate_dataset.py \
   --b-ppe -10 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -290,14 +400,14 @@ python npe/generate_dataset.py \
   --b-ppe -11 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -312,14 +422,14 @@ python npe/generate_dataset.py \
   --b-ppe -12 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
@@ -334,14 +444,14 @@ python npe/generate_dataset.py \
   --b-ppe -13 \
   --n-ppe 1 \
   --minus-gr \
-  --m1-min $M_MIN --m1-max $M_MAX \
-  --m2-min $M_MIN --m2-max $M_MAX \
-  --chi1z-min $CHI_MIN --chi1z-max $CHI_MAX \
-  --chi2z-min $CHI_MIN --chi2z-max $CHI_MAX \
-  --l1-min $L_MIN --l1-max $L_MAX \
-  --l2-min $L_MIN --l2-max $L_MAX \
-  --cq1-min $CQ_MIN --cq1-max $CQ_MAX \
-  --cq2-min $CQ_MIN --cq2-max $CQ_MAX \
+  --m1-min $M_MIN_1 --m1-max $M_MAX_1 \
+  --m2-min $M_MIN_2 --m2-max $M_MAX_2 \
+  --chi1z-min $CHI_MIN_1 --chi1z-max $CHI_MAX_1 \
+  --chi2z-min $CHI_MIN_2 --chi2z-max $CHI_MAX_2 \
+  --l1-min $L_MIN_1 --l1-max $L_MAX_1 \
+  --l2-min $L_MIN_2 --l2-max $L_MAX_2 \
+  --cq1-min $CQ_MIN_1 --cq1-max $CQ_MAX_1 \
+  --cq2-min $CQ_MIN_2 --cq2-max $CQ_MAX_2 \
   --fmin $F_MIN --fmax $F_MAX \
   --num-freqs 640 \
   --logspace-freqs \
